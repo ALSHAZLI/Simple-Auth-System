bb1de0a32beaf1e1d2056a581f8d296f93abdaef
Apolgasim <81713835+ALSHAZLI@users.noreply.github.com>
2021-08-24 15:26:17 +0300

    Update conf.js

--- conf.js
+++ conf.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MONGO_URL:"mongodb://yourUSERNAME:yourPASS@cluster0-shard-00-00.iczji.mongodb.net:27017,cluster0-shard-00-01.iczji.mongodb.net:27017,cluster0-shard-00-02.iczji.mongodb.net:27017/yourDBNAME?ssl=true&replicaSet=atlas-pcxqif-shard-0&authSource=admin&retryWrites=true&w=majority"
+    MONGO_URL:"mongodb://BLABLABAL:BLABLABAL@cluster0-shard-00-00.iczji.mongodb.net:27017,cluster0-shard-00-01.iczji.mongodb.net:27017,cluster0-shard-00-02.iczji.mongodb.net:27017/BLABLABAL?ssl=true&replicaSet=atlas-pcxqif-shard-0&authSource=admin&retryWrites=true&w=majority"
 }
